ce21971450d66fcbf56004491500ebc1abcf33a3
Alex Scarlett <alex.scarlett91@gmail.com>
2018-03-18 20:58:16 -0400

    Fixed fadeIn and fadeOut of index

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+  $('#homepage-top').css('display', 'none');
   $('#main-content').css('display', 'none');
+  $('#homepage-top').fadeIn(2000);
   $('.btn').click(function(event) {
     event.preventDefault();
-    $('#homepage-top').fadeOut(800);
+    $('#homepage-top').fadeOut(300);
     $('#main-content').fadeIn(3000);
   });
 });
