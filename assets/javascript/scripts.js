e236c8cf8287b7813dc9b115eb02bc4edc453732
Alex Scarlett <alex.scarlett91@gmail.com>
2018-03-19 18:05:44 -0400

    Fixed comments in js file

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
   // hide page with display none so fade in can happen
   $('body').css('display', 'none');
 
-  // fade in all pages over 2 sec
+  // fade in any new page over 2 sec
   $('body').fadeIn(2000);
 
-  // click event on link
+  // click event on links to fade out current page and fade in the linked page
   $('a.transition').click(function(event) {
     event.preventDefault();
-    // set newLocation variable for newpage function
-    linkLocation = this.href;
+    // set newLocation variable for newpage function, set it equal to the page that is linked to
+    pageLink = this.href;
     // fade out the entire page over 1 sec
     $('body').fadeOut(1000, newPage);
   });
 
   // change window location to the new page being linked
   function newPage() {
-    window.location = linkLocation;
+    window.location = pageLink;
   }
 });
