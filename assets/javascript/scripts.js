fa270bb7505f398d8f016c17d230f34eeff0786f
Alex Scarlett <alex.scarlett91@gmail.com>
2018-03-27 19:39:24 -0400

    Almost done reworking entire portfolio. Just need to add contact section with social links

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,30 +1,39 @@
 $(document).ready(function() {
-  // hide navbar
-  $('#navbar').css('display', 'none');
+  // hide navbar and footer
+  $('#navbar, #footer').css('display', 'none');
 
   // hide body and fade in
   $('body').css('display', 'none');
 
   $('body').fadeIn(2500);
 
-  // click event on links to fade out current page and fade in the linked page
-  $('a.transition').click(function(event) {
-    event.preventDefault();
-
-    window.location = this.href;
-
-    $('body').fadeOut(2000);
-  });
-
-  // scroll to content container me
-  $('.scroll-down').click(function() {
+  $('.scroll-about').click(function() {
     $('html, body').animate(
       {
-        scrollTop: $('.container').offset().top
+        scrollTop: $('#about-full').offset().top
       },
       1500
     );
-    $('#homepage-full').fadeOut(1500);
-    $('#navbar').fadeIn(1500);
+    //$('#homepage-full').fadeOut(1500);
+    // $('#navbar').fadeIn(1500);
+  });
+
+  $('.scroll-projects').click(function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('#portfolio').offset().top
+      },
+      1500
+    );
+  });
+
+  $('.scroll-contact').click(function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('#contact').offset().top
+      },
+      1500
+    );
+    $('#footer').fadeIn(1500);
   });
 });
