639b3fa60691a2755a1a6a2b9341792d8d1a22c8
Alex Scarlett <alex.scarlett91@gmail.com>
2018-03-27 10:41:14 -0400

    Fixed JS fadein

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,28 +1,22 @@
 $(document).ready(function() {
   // hide navbar
-  $('#homepage-full ,#navbar').css('display', 'none');
+  $('#navbar').css('display', 'none');
 
-  $('#homepage-full').fadeIn(1500);
+  // hide body and fade in
+  $('body').css('display', 'none');
 
-  // fade in any new page over 2 sec
-
-  // $('#portfolio-page, #').fadeIn(2000);
+  $('body').fadeIn(2500);
 
   // click event on links to fade out current page and fade in the linked page
-  $('a').click(function(event) {
+  $('a.transition').click(function(event) {
     event.preventDefault();
-    // set newLocation variable for newpage function, set it equal to the page that is linked to
-    pageLink = this.href;
-    // fade out the entire page over 1 sec
-    $('body').fadeOut(1500, newPage);
+
+    window.location = this.href;
+
+    $('body').fadeOut(2000);
   });
 
-  // change window location to the new page being linked
-  function newPage() {
-    window.location = pageLink;
-  }
-
-  // scroll to about me
+  // scroll to content container me
   $('.scroll-down').click(function() {
     $('html, body').animate(
       {
