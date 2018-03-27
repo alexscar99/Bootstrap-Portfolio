9116370c3f6343046963688afa4c74393cb5721c
Alex Scarlett <alex.scarlett91@gmail.com>
2018-03-27 03:04:11 -0400

    In the process of redoing portfolio

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,21 +1,36 @@
 $(document).ready(function() {
-  // hide page with display none so fade in can happen
-  $('body').css('display', 'none');
+  // hide navbar
+  $('#homepage-full ,#navbar').css('display', 'none');
+
+  $('#homepage-full').fadeIn(1500);
 
   // fade in any new page over 2 sec
-  $('body').fadeIn(2000);
+
+  // $('#portfolio-page, #').fadeIn(2000);
 
   // click event on links to fade out current page and fade in the linked page
-  $('a.transition').click(function(event) {
+  $('a').click(function(event) {
     event.preventDefault();
     // set newLocation variable for newpage function, set it equal to the page that is linked to
     pageLink = this.href;
     // fade out the entire page over 1 sec
-    $('body').fadeOut(1000, newPage);
+    $('body').fadeOut(1500, newPage);
   });
 
   // change window location to the new page being linked
   function newPage() {
     window.location = pageLink;
   }
+
+  // scroll to about me
+  $('.scroll-down').click(function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('.container').offset().top
+      },
+      1500
+    );
+    $('#homepage-full').fadeOut(1500);
+    $('#navbar').fadeIn(1500);
+  });
 });
