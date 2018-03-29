7c75c29e3090d6080c99c3a514b2f299504de165
Alex Scarlett <alex.scarlett91@gmail.com>
2018-03-29 10:15:52 -0400

    Changed jQuery landing page

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,8 +1,32 @@
 $(document).ready(function() {
   // hide body and fade in
-  $('body').css('display', 'none');
+  // $('body').css('display', 'none');
 
-  $('body').fadeIn(2500);
+  // $('#homepage-full').fadeIn(2500);
+  $('.scroll-about').hide();
+
+  $(function() {
+    var string =
+      "Hi, I'm Alex Scarlett. I'm a full-stack web developer who enjoys coding anything and everything.";
+    var lettersArray = jQuery.map(string.split(''), function(letter) {
+      return $('<span>' + letter + '</span>');
+    });
+
+    var dest = $('#homepage-heading');
+
+    var counter = 0;
+    var i = setInterval(function() {
+      lettersArray[counter]
+        .appendTo(dest)
+        .hide()
+        .fadeIn(75);
+      counter += 1;
+      if (counter >= lettersArray.length) {
+        clearInterval(i);
+        $('.scroll-about').fadeIn(2000);
+      }
+    }, 75);
+  });
 
   // scroll to about section
   $('.scroll-about').click(function() {
