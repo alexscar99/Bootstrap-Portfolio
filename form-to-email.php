e026cb2d102cd9189403d76d60862a2a09b5422c
Alex Scarlett <alex.scarlett91@gmail.com>
2018-04-05 14:25:56 -0400

    Working on contact form email with php file

--- form-to-email.php
+++ form-to-email.php
@@ -9,17 +9,17 @@ $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
 //Validate first
-if(empty($name)|| empty($visitor_email)) 
-{
-    echo "Name and email are mandatory!";
-    exit;
-}
+// if(empty($name)|| empty($visitor_email)) 
+// {
+//     echo "Name and email are mandatory!";
+//     exit;
+// }
 
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
+// if(IsInjected($visitor_email))
+// {
+//     echo "Bad email value!";
+//     exit;
+// }
 
 $email_from = 'alex.scarlett91@gmail.com';
 $email_subject = "New Form submission";
@@ -32,30 +32,31 @@ $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: index.html#contact');
+header('Location: index.html');
+?>
 
 
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
-    return true;
-  }
-  else
-    {
-    return false;
-  }
-}
+<!-- // Function to validate against any email injection attempts
+ function IsInjected($str)
+ {
+   $injections = array('(\n+)',
+               '(\r+)',
+               '(\t+)',
+               '(%0A+)',
+               '(%0D+)',
+               '(%08+)',
+               '(%09+)'
+               );
+   $inject = join('|', $injections);
+   $inject = "/$inject/i";
+   if(preg_match($inject,$str))
+     {
+     return true;
+   }
+   else
+     {
+     return false;
+   }
+ }
    
-?> 
+ ?>  -->
